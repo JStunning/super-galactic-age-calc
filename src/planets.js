e21373cb34ae2204f5cb4d383198b7681626381b
Jack Dunning <JackStunning9001@gmail.com>
2020-02-07 15:55:31 -0800

    Add reset button

--- src/planets.js
+++ src/planets.js
@@ -7,25 +7,25 @@ export class Planets {
   mercuryAge() {
     this.age = parseFloat((this.age / 0.24).toFixed(2));
     this.lifeExp = parseFloat((this.lifeExp / 0.24).toFixed(2));
-    this.yearsLeft = parseFloat(this.lifeExp - this.age);
+    this.yearsLeft = parseFloat(this.lifeExp - this.age).toFixed(2);
   }
 
   venusAge() {
     this.age = parseFloat((this.age / 0.62).toFixed(2))
     this.lifeExp = parseFloat((this.lifeExp / 0.62).toFixed(2));
-    this.yearsLeft = parseFloat(this.lifeExp - this.age);
+    this.yearsLeft = parseFloat(this.lifeExp - this.age).toFixed(2);
   }
 
   marsAge() {
     this.age = parseFloat((this.age / 1.88).toFixed(2));
     this.lifeExp = parseFloat((this.lifeExp / 1.88).toFixed(2));
-    this.yearsLeft = parseFloat(this.lifeExp - this.age);
+    this.yearsLeft = parseFloat(this.lifeExp - this.age).toFixed(2);
   }
 
   jupiterAge() {
     this.age = parseFloat((this.age / 11.86).toFixed(2));
     this.lifeExp = parseFloat((this.lifeExp / 11.86).toFixed(2));
-    this.yearsLeft = parseFloat(this.lifeExp - this.age);
+    this.yearsLeft = parseFloat(this.lifeExp - this.age).toFixed(2);
   }  
 
 }

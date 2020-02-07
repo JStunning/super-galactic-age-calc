4aa34370dc5e26bcad54e9b04a3b47be5733b6ac
Jack Dunning <JackStunning9001@gmail.com>
2020-02-07 14:54:14 -0800

    Add front end to show our back end

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
+import $ from 'jQuery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { Planets } from './planets';
+
+$(document).ready(function(){
+
+  $("#ageButton").click(function(event){
+    event.preventDefault();
+
+    const age = $("#age").val();
+    const newPlanet = new Planets(age);
+
+    $("#ageOutput").text(age);
+    $("#ageOutput").append(`<br> Mercury Age: ${newPlanet.mercuryAge(), newPlanet.age}`);
+    $("#ageOutput").append(`<br> Venus Age: ${newPlanet.age = age, newPlanet.venusAge(), newPlanet.age}`);
+    $("#ageOutput").append(`<br> Mars Age: ${newPlanet.age = age, newPlanet.marsAge(), newPlanet.age}`);
+    $("#ageOutput").append(`<br> Jupiter Age: ${newPlanet.age = age, newPlanet.jupiterAge(), newPlanet.age}`);
+
+
+
+  });
+
+});
 

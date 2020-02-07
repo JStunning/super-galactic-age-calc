00b66afda8411699afeb4ca1ffb9f197d40e16cc
Jack Dunning <JackStunning9001@gmail.com>
2020-02-07 09:31:31 -0800

    Mercury test done

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //new line
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');  // new line
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');  
 
 
 module.exports = {
@@ -10,13 +10,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-source-map',  // new line
-  devServer: {                 // new line
-  contentBase: './dist'      // new line
+  devtool: 'eval-source-map',  
+  devServer: {                 
+  contentBase: './dist'      
 },
   plugins: [
-     new UglifyJsPlugin({ sourceMap: true }),    // new line
-    new CleanWebpackPlugin(),   // new line
+     new UglifyJsPlugin({ sourceMap: true }),    
+    new CleanWebpackPlugin(),   
     new HtmlWebpackPlugin({
       title: '',
       template: './src/index.html',
